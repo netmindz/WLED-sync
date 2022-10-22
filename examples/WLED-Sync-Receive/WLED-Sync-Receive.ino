0bdd9a1b43a326a92deaeeb0091601dd6de097c8
Will Tatam <will@netmindz.net>
2022-10-22 18:28:55 +0100

    Remove OTA from WLED-Sync-Receive

--- examples/WLED-Sync-Receive/WLED-Sync-Receive.ino
+++ examples/WLED-Sync-Receive/WLED-Sync-Receive.ino
@@ -8,7 +8,6 @@
 #else // ESP32
 #include <WiFi.h>
 #endif
-#include <ArduinoOTA.h>
 #include <WLED-sync.h> // https://github.com/netmindz/WLED-sync
 
 #include "wifi.h"
@@ -33,35 +32,6 @@ void setup() {
   }
   Serial.print("\nConnected! IP address: ");
   Serial.println(WiFi.localIP());
-
-  ArduinoOTA.setHostname("WLED-MSGEQ7");
-  ArduinoOTA
-  .onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  })
-  .onEnd([]() {
-    Serial.println("\nEnd");
-  })
-  .onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  })
-  .onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-
-  ArduinoOTA.begin();
   
   sync.begin();
 
@@ -69,7 +39,6 @@ void setup() {
 }
 
 void loop() {
-  ArduinoOTA.handle();
 
   if (sync.read()) {
     for (int b = 0; b < NUM_GEQ_CHANNELS; b++) {
