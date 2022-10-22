fc533bad5f2835e1aca6a82d88d96dcf6f431d5d
Will Tatam <will@netmindz.net>
2022-10-22 13:28:33 +0100

    Add example of sending data from MSGEQ7 chip

---
+++ examples/WLED-MSGEQ7-Sender/README.md
@@ -0,0 +1,8 @@
+# WLED-MSGEQ7-Sender
+Read data from MSGEQ7 chip and send to wifi network in WLED compatible format, just use the Audio Responsive fork of WLED and set your device to receive 
+
+Create a wifi.h file with your wifi details
+
+#define SECRET_SSID "";  /* Replace with your SSID */
+
+#define SECRET_PSK "";   /* Replace with your WPA2 passphrase */
---
+++ examples/WLED-MSGEQ7-Sender/WLED-MSGEQ7-Sender.ino
@@ -0,0 +1,118 @@
+/*
+   Read audio data from MSGEQ7 chip and send as UDP data in the same format as used in https://github.com/atuline/WLED
+   This allows for sending FFT data from either ESP8266 or ESP32 to other devices running the audio responsive WLED
+*/
+
+#if defined(ESP8266) // ESP8266
+#include <ESP8266WiFi.h>
+#else // ESP32
+#include <WiFi.h>
+#endif
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <WLED-sync.h>
+
+#include "wifi.h"
+// Create file with the following
+// *************************************************************************
+// #define SECRET_SSID "";  /* Replace with your SSID */
+// #define SECRET_PSK "";   /* Replace with your WPA2 passphrase */
+// *************************************************************************
+const char ssid[] = SECRET_SSID;
+const char passphrase[] = SECRET_PSK;
+
+
+// MSGEQ7
+#include "MSGEQ7.h"
+#define pinAnalogLeft A0 // "VP"
+#define pinAnalogRight A0
+#define pinReset 22
+#define pinStrobe 19
+#define MSGEQ7_INTERVAL ReadsPerSecond(50)
+#define MSGEQ7_SMOOTH false
+
+CMSGEQ7<MSGEQ7_SMOOTH, pinReset, pinStrobe, pinAnalogLeft, pinAnalogRight> MSGEQ7;
+
+WiFiUDP fftUdp;
+
+uint16_t audioSyncPort = 20000;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, passphrase);
+  Serial.print("Connecting to WiFi ");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.print("\nConnected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+#ifndef ESP8266
+  fftUdp.beginMulticast(IPAddress(239, 0, 0, 1), audioSyncPort);
+#else
+  fftUdp.beginMulticast(WiFi.localIP(), IPAddress(239, 0, 0, 1), audioSyncPort);
+#endif
+  // This will set the IC ready for reading
+  MSGEQ7.begin();
+
+  ArduinoOTA.setHostname("WLED-MSGEQ7");
+  ArduinoOTA
+  .onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  })
+  .onEnd([]() {
+    Serial.println("\nEnd");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  // Analyze without delay every interval
+  bool newReading = MSGEQ7.read(MSGEQ7_INTERVAL);
+  if (newReading) {
+    audioSyncPacket transmitData;
+
+    for (int b = 0; b < 14; b = b + 2) {
+      int val = MSGEQ7.get((b / 2));
+      val = mapNoise(val);
+      Serial.printf("%u ", val);
+      transmitData.fftResult[b] = val;
+      transmitData.fftResult[(b + 1)] = val;
+    }
+    int v = map(MSGEQ7.getVolume(), 0, 255, 0, 1023);
+    // TODO: not sure any of these are right
+    // transmitData.sample = v; // Current sample
+    // transmitData.sampleAgc = v; // Our AGC sample
+    // transmitData.sampleAvg = v; // Gain adjusted sample value
+    
+    Serial.println();
+
+    fftUdp.beginMulticastPacket();
+    fftUdp.write(reinterpret_cast<uint8_t *>(&transmitData), sizeof(transmitData));
+    fftUdp.endPacket();
+  }
+
+}
