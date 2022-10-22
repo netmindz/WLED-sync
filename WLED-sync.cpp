d7826df6d4f42a7d8c81265da4867c67ce413a37
Will Tatam <will@netmindz.net>
2022-10-22 16:18:31 +0100

    Add work in progress on supporting receiving data

--- WLED-sync.cpp
+++ WLED-sync.cpp
@@ -1,9 +1,12 @@
 #include "WLED-sync.h"
 
     
-  bool isValidUdpSyncVersion(char header[6]) {
-    return (header == UDP_SYNC_HEADER);
-  }
+static bool isValidUdpSyncVersion(const char *header) {
+  return strncmp_P(header, PSTR(UDP_SYNC_HEADER), 6) == 0;
+}
+static bool isValidUdpSyncVersion_v1(const char *header) {
+  return strncmp_P(header, PSTR(UDP_SYNC_HEADER_v1), 6) == 0;
+}
 
 WLEDSync::WLEDSync() {
 }
@@ -21,3 +24,102 @@ void WLEDSync::send(audioSyncPacket transmitData) {
     fftUdp.write(reinterpret_cast<uint8_t *>(&transmitData), sizeof(transmitData));
     fftUdp.endPacket();
 }
+
+bool WLEDSync::read()   // check & process new data. return TRUE in case that new audio data was received. 
+{
+  if (!udpSyncConnected) return false;
+  bool haveFreshData = false;
+
+  size_t packetSize = fftUdp.parsePacket();
+  if (packetSize > 5) {
+    //DEBUGSR_PRINTLN("Received UDP Sync Packet");
+    uint8_t fftBuff[packetSize];
+    fftUdp.read(fftBuff, packetSize);
+
+    // VERIFY THAT THIS IS A COMPATIBLE PACKET
+    if (packetSize == sizeof(audioSyncPacket) && (isValidUdpSyncVersion((const char *)fftBuff))) {
+      WLEDSync::decodeAudioData(packetSize, fftBuff);
+      //DEBUGSR_PRINTLN("Finished parsing UDP Sync Packet v2");
+      haveFreshData = true;
+      receivedFormat = 2;
+    }
+    else {
+      if (packetSize == sizeof(audioSyncPacket_v1) && (isValidUdpSyncVersion_v1((const char *)fftBuff))) {
+        WLEDSync::decodeAudioData_v1(packetSize, fftBuff);
+        //DEBUGSR_PRINTLN("Finished parsing UDP Sync Packet v1");
+        haveFreshData = true;
+        receivedFormat = 1;
+      } else receivedFormat = 0; // unknown format
+    }
+  }
+  else if(packetSize > 0) {
+    Serial.println("packetSize=" + packetSize);
+  }
+  return haveFreshData;
+}
+
+void WLEDSync::decodeAudioData_v1(int packetSize, uint8_t *fftBuff) {
+  audioSyncPacket_v1 *receivedPacket = reinterpret_cast<audioSyncPacket_v1*>(fftBuff);
+  // update samples for effects
+  volumeSmth   = fmaxf(receivedPacket->sampleAgc, 0.0f);
+  volumeRaw    = volumeSmth;   // V1 format does not have "raw" AGC sample
+  // update internal samples
+  sampleRaw    = fmaxf(receivedPacket->sampleRaw, 0.0f);
+  sampleAvg    = fmaxf(receivedPacket->sampleAvg, 0.0f);;
+  sampleAgc    = volumeSmth;
+  rawSampleAgc = volumeRaw;
+  multAgc      = 1.0f;   
+  // Only change samplePeak IF it's currently false.
+  // If it's true already, then the animation still needs to respond.
+  autoResetPeak();
+  if (!samplePeak) {
+        samplePeak = receivedPacket->samplePeak >0 ? true:false;
+        if (samplePeak) timeOfPeak = millis();
+        //userVar1 = samplePeak;
+  }
+  //These values are only available on the ESP32
+  for (int i = 0; i < NUM_GEQ_CHANNELS; i++) fftResult[i] = receivedPacket->fftResult[i];
+  my_magnitude  = fmaxf(receivedPacket->FFT_Magnitude, 0.0);
+  FFT_Magnitude = my_magnitude;
+  FFT_MajorPeak = constrain(receivedPacket->FFT_MajorPeak, 1.0, 11025.0);  // restrict value to range expected by effects
+}
+
+void WLEDSync::decodeAudioData(int packetSize, uint8_t *fftBuff) {
+  audioSyncPacket *receivedPacket = reinterpret_cast<audioSyncPacket*>(fftBuff);
+  // update samples for effects
+  volumeSmth   = fmaxf(receivedPacket->sampleSmth, 0.0f);
+  volumeRaw    = fmaxf(receivedPacket->sampleRaw, 0.0f);
+  // update internal samples
+  sampleRaw    = volumeRaw;
+  sampleAvg    = volumeSmth;
+  rawSampleAgc = volumeRaw;
+  sampleAgc    = volumeSmth;
+  multAgc      = 1.0f;   
+  // Only change samplePeak IF it's currently false.
+  // If it's true already, then the animation still needs to respond.
+  autoResetPeak();
+  if (!samplePeak) {
+        samplePeak = receivedPacket->samplePeak >0 ? true:false;
+        if (samplePeak) timeOfPeak = millis();
+        //userVar1 = samplePeak;
+  }
+  //These values are only available on the ESP32
+  for (int i = 0; i < NUM_GEQ_CHANNELS; i++) {
+    fftResult[i] = receivedPacket->fftResult[i];
+    Serial.printf("%u ", fftResult[i]);
+  }
+  Serial.println(" 100");
+
+  my_magnitude  = fmaxf(receivedPacket->FFT_Magnitude, 0.0f);
+  FFT_Magnitude = my_magnitude;
+  FFT_MajorPeak = constrain(receivedPacket->FFT_MajorPeak, 1.0f, 11025.0f);  // restrict value to range expected by effects
+}
+
+void WLEDSync::autoResetPeak(void) {
+  uint16_t MinShowDelay = 50; // MAX(50, strip.getMinShowDelay());  // Fixes private class variable compiler error. Unsure if this is the correct way of fixing the root problem. -THATDONFC
+  if (millis() - timeOfPeak > MinShowDelay) {          // Auto-reset of samplePeak after a complete frame has passed.
+    samplePeak = false;
+    if (audioSyncEnabled == 0) udpSamplePeak = false;  // this is normally reset by transmitAudioData
+  }
+}
+
--- WLED-sync.h
+++ WLED-sync.h
@@ -8,8 +8,10 @@
 #include "Arduino.h"
 #include <WiFiUdp.h>
 
+#define UDP_SYNC_HEADER_v1 "00001"
 #define UDP_SYNC_HEADER "00002"
 #define UDP_SYNC_PORT 11988
+#define NUM_GEQ_CHANNELS 16                     // number of frequency channels. Don't change !!
 
 struct audioSyncPacket {
   char    header[6];      //  06 Bytes
@@ -22,16 +24,56 @@ struct audioSyncPacket {
   float  FFT_MajorPeak;   //  04 Bytes
 };
 
+// old "V1" audiosync struct - 83 Bytes - for backwards compatibility
+struct audioSyncPacket_v1 {
+  char header[6];         //  06 Bytes
+  uint8_t myVals[32];     //  32 Bytes
+  int sampleAgc;          //  04 Bytes
+  int sampleRaw;          //  04 Bytes
+  float sampleAvg;        //  04 Bytes
+  bool samplePeak;        //  01 Bytes
+  uint8_t fftResult[16];  //  16 Bytes
+  double FFT_Magnitude;   //  08 Bytes
+  double FFT_MajorPeak;   //  08 Bytes
+};
+
 class WLEDSync {
 
   private:
     WiFiUDP fftUdp;
+    bool udpSyncConnected;
+
+    bool audioSyncEnabled = true; // not actually an option, but kept here to keep code in sync with UserMod
+
+    // peak detection
+    bool samplePeak = false;      // Boolean flag for peak - used in effects. Responding routine may reset this flag. Auto-reset after strip.getMinShowDelay()
+    unsigned long timeOfPeak = 0; // time of last sample peak detection.
+    bool udpSamplePeak = false;   // Boolean flag for peak. Set at the same tiem as samplePeak, but reset by transmitAudioData
+
+
+    void decodeAudioData(int packetSize, uint8_t *fftBuff);
+    void decodeAudioData_v1(int packetSize, uint8_t *fftBuff);
+    void autoResetPeak(void);
 
   public:
-    
+    int receivedFormat = -1;            // last received UDP sound sync format - 0=none, 1=v1 (0.13.x), 2=v2 (0.14.x)
+    uint8_t fftResult[NUM_GEQ_CHANNELS]= {0};// Our calculated freq. channel result table to be used by effects
+    float FFT_MajorPeak = 1.0f;   // FFT: strongest (peak) frequency
+    float FFT_Magnitude = 0.0f;   // FFT: volume (magnitude) of peak frequency
+    // variables used in effects
+    float    volumeSmth = 0.0f;   // either sampleAvg or sampleAgc depending on soundAgc; smoothed sample
+    int16_t  volumeRaw = 0;       // either sampleRaw or rawSampleAgc depending on soundAgc
+    int16_t  sampleRaw = 0;       // Current sample. Must only be updated ONCE!!! (amplified mic value by sampleGain and inputLevel)
+    float    my_magnitude =0.0f;     // FFT_Magnitude, scaled by multAgc
+    int16_t  rawSampleAgc = 0;    // not smoothed AGC sample
+    float    sampleAgc = 0.0f;    // Smoothed AGC sample
+    float    multAgc = 1.0f;      // sample * multAgc = sampleAgc. Our AGC multiplier
+    float    sampleAvg = 0.0f;    // Smoothed Average sample - sampleAvg < 1 means "quiet" (simple noise gate)
+
     WLEDSync();
 
     void begin();
     void send(audioSyncPacket transmitData);
+    bool read();
 };
 #endif
